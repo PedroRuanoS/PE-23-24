347a4a12d7d9e2269231f9b10428717ff189829f
Pedro Ruano Silveira <pruanosilveira@gmail.com>
2024-06-04 04:38:31 +0100

    proj finished

--- project/ex1/ex1.R
+++ project/ex1/ex1.R
@@ -2,18 +2,18 @@ library(ggplot2)
 theme_set(theme_gray())
 
 url <- 'https://web.tecnico.ulisboa.pt/~paulo.soares/pe/projeto/Paises_PIB_ICH.csv'
-dados <- read.csv(url, check.names = FALSE)
-continentes <- c("Europe", "Americas")
-paises_especificos <- c("Lithuania", "Iceland", "United States", "Saint Lucia")
+data <- read.csv(url, check.names = FALSE)
+continents <- c("Europe", "Americas")
+countries <- c("Lithuania", "Iceland", "United States", "Saint Lucia")
 
-dados |>
-  subset(Continent %in% continentes) |>
+data |>
+  subset(Continent %in% continents) |>
   ggplot() +
   geom_point(aes(GDP, HCI, color = Continent)) +
   scale_x_log10() +
-  geom_text(data = subset(dados, Country %in% paises_especificos),
+  geom_text(data = subset(data, Country %in% countries),
             aes(x = GDP, y = HCI, label = Country),
             color = "black") +
   labs(title = paste("Human Capital Index vs GDP per capita"),
-       x = "GDP per capita",
-       y = "Human Capital Index")
+      x = "GDP per capita",
+      y = "Human Capital Index")
--- project/ex2/ex2.R
+++ project/ex2/ex2.R
@@ -2,10 +2,10 @@ library(ggplot2)
 theme_set(theme_gray())
 
 url <- 'https://web.tecnico.ulisboa.pt/~paulo.soares/pe/projeto/master.csv'
-suicidios <- read.csv(url)
-suicidios_selec <- subset(suicidios, year == 2002 & age == "55-74 years")
+suicides_data <- read.csv(url)
+suicides_selec <- subset(suicides_data, year == 2002 & age == "55-74 years")
 
-ggplot(suicidios_selec) +
+ggplot(suicides_selec) +
   geom_boxplot(aes(x = sex, y = suicides.100k.pop)) +
   labs(title = paste0("Nº of suicides per 100k habitants (Women vs Men)"),
        y = "Nº of suicides/100k habitants")
